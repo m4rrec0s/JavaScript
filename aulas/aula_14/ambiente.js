074ab1f46330eb6a8ef1e58e22ce036438bd61e7
m4rrec0s <mine.rique.games@gmail.com>
2023-11-06 11:52:27 -0300

    Update ambiente.js

--- aulas/aula_14/ambiente.js
+++ aulas/aula_14/ambiente.js
@@ -4,3 +4,7 @@ console.log(num)
 console.log(`O vetor tem ${num.length} posições!`)
 console.log(`O primeiro valor do vetor é ${num[0]}`)
 console.log(`Organizando os valores fica: ${num.sort()}`)
+
+for (let c = 0; c < num.length; c++) {
+    console.log(`A posição ${c} tem o valor ${num[c]}`)
+}
