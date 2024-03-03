088efba17634c303a2ab96436cb39cd30b8aa2b1
Marcos Henrique Araújo <mine.rique.games@gmail.com>
2024-03-03 19:00:32 -0300

    ...

--- todolist/script.js
+++ todolist/script.js
@@ -8,8 +8,10 @@ checkboxes.forEach(function(checkbox) {
         var parentDiv = this.closest(".contentTask"); // Encontre a div pai mais próxima com a classe .contentTask
         if (this.checked) {
             parentDiv.style.backgroundColor = "var(--buttonColor)"; // Altera a cor de fundo da div pai quando a checkbox é marcada
+            parentDiv.style.color = "var(--back)"
         } else {
             parentDiv.style.backgroundColor = "var(--task)"; // Retorna à cor original quando a checkbox é desmarcada
+            parentDiv.style.color = "var(--font)";
         }
     });
 });
