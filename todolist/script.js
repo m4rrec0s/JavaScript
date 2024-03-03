afdf583c73a60f951e3bffe6c2016fae1d5217c9
Marcos Henrique Araújo <mine.rique.games@gmail.com>
2024-03-03 14:57:17 -0300

    adicionando funcionalidades com JavaScript

---
+++ todolist/script.js
@@ -0,0 +1,19 @@
+var addNewTask = document.getElementById("addNewTask")
+var caixa = document.getElementById("caixa_1")
+
+var checkboxes = document.querySelectorAll(".checkbox");
+
+checkboxes.forEach(function(checkbox) {
+    checkbox.addEventListener("click", function() {
+        var parentDiv = this.closest(".contentTask"); // Encontre a div pai mais próxima com a classe .contentTask
+        if (this.checked) {
+            parentDiv.style.backgroundColor = "var(--buttonColor)"; // Altera a cor de fundo da div pai quando a checkbox é marcada
+        } else {
+            parentDiv.style.backgroundColor = "var(--task)"; // Retorna à cor original quando a checkbox é desmarcada
+        }
+    });
+});
+
+addNewTask.addEventListener("click", function() {
+
+})
