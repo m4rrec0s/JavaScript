561184c1ff8d8abdf43cf81524979f8be71d9167
Marcos Henrique Araújo <mine.rique.games@gmail.com>
2024-03-17 16:59:44 -0300

    ...

--- c-bnb/js/script.js
+++ c-bnb/js/script.js
@@ -9,7 +9,13 @@ window.addEventListener('DOMContentLoaded', () => {
       const modalTitle = document.getElementById('modal-content-title');
       const modalImg = document.getElementById('modal-content-img');
       const modalDescription = document.getElementById('modal-content-description');
-      const modalSub = document.getElementById('modal-content-sub')
+      const modalSub = document.getElementById('modal-content-sub');
+      const modalFunctions = document.getElementById('modal-content-functions');
+      const modalComposition = document.getElementById('modal-container-composition');
+      const modalMainFunctions = document.getElementById('modal-container-mainFunctions');
+      const modalImportancePr = document.getElementById('modal-container-importance-pr');
+      const modalImportanceUl = document.getElementById('modal-container-importance-ul');
+      const modalLinks = document.getElementById('modal-container-links');
 
       const exitBtn = document.querySelector('.fa-xmark');
 
@@ -25,24 +31,78 @@ window.addEventListener('DOMContentLoaded', () => {
               modalTitle.textContent = assuntoData.title;
               modalImg.src = assuntoData.picture;
               modalDescription.textContent = assuntoData.description;
-              if (Array.isArray(assuntoData.subDescription)) {
-                modalSub.innerHTML = '<ul>';
-                assuntoData.subDescription.forEach(desc => {
-                  modalSub.innerHTML += `<li>${desc}</li>`;
+              modalSub.textContent = assuntoData.subDescription;
+
+              if (Array.isArray(assuntoData.functions)) {
+                modalFunctions.innerHTML = '<ul>';
+                assuntoData.functions.forEach(desc => {
+                  modalFunctions.innerHTML += `<li>${desc}</li>`;
                 });
-                modalSub.innerHTML += '</ul>';
+                modalFunctions.innerHTML += '</ul>';
               } else {
-                modalSub.textContent = assuntoData.subDescription;
+                modalFunctions.textContent = assuntoData.functions;
               }
 
-              modal.style.display = 'block';
+              if (Array.isArray(assuntoData.composition)) {
+                modalComposition.innerHTML = '<ul>';
+                assuntoData.composition.forEach(desc => {
+                  modalComposition.innerHTML += `<li>${desc}</li>`;
+                });
+                modalComposition.innerHTML += '</ul>';
+              } else {
+                modalComposition.textContent = assuntoData.composition;
+              }
+
+              if (Array.isArray(assuntoData.mainFunctions)) {
+                modalMainFunctions.innerHTML = '<ul>';
+                assuntoData.mainFunctions.forEach(desc => {
+                  modalMainFunctions.innerHTML += `<li>${desc}</li>`;
+                });
+                modalMainFunctions.innerHTML += '</ul>';
+              } else {
+                modalMainFunctions.textContent = assuntoData.mainFunctions;
+              }
+
+
+              modalImportancePr.textContent = assuntoData.importancetxt;
+              if (Array.isArray(assuntoData.importance)) {
+                modalImportanceUl.innerHTML = '<ul>';
+                assuntoData.importance.forEach(desc => {
+                  modalImportanceUl.innerHTML += `<li>${desc}</li>`;
+                });
+                modalImportanceUl.innerHTML += '</ul>';
+              } else {
+                modalImportanceUl.textContent = assuntoData.importance;
+              }
+
+              if (Array.isArray(assuntoData.links)) {
+                modalLinks.innerHTML = '<div>';
+                assuntoData.links.forEach(desc => {
+                  modalLinks.innerHTML += `<a href="${desc.url}" target="_blank"> ✓ ${desc.title}</a>`;
+                });
+                modalLinks.innerHTML += '</div>';
+              } else {
+                modalLinks.textContent = assuntoData.links;
+              }
+
+              setTimeout(() => modal.style.display = 'block', 20);
+              setTimeout(() => modal.classList.add('open'), 10);
+              
             }
           }
         });
       });
-
+      
       exitBtn.addEventListener('click', () => {
-        modal.style.display = 'none';
+        modal.classList.remove('open');
+        modal.classList.add('closed');
+      });
+      
+      modal.addEventListener('animationend', () => {
+        if (modal.classList.contains('closed')) {
+          modal.style.display = 'none';
+          modal.classList.remove('closed');
+        }
       });
     })
     .catch(error => {
